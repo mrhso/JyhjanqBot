9e31a6efac26a48ea8a4939d0f00e7490fcb6811
mrhso <liu.songquan@live.cn>
2019-01-23 14:17:26 +0800

    程序本体

---
+++ config.example.js
@@ -0,0 +1,10 @@
+// 请参照注释进行设定
+// 设定好之后，请将档案更名为 config.js
+
+module.exports = {
+    "host": "127.0.0.1", // 酷 Q 所在环境的 IP
+    "port": 11235,       // 酷 Q 的通信端口
+    "isGroup": true,     // true 为群，false 为私聊
+    "id": "10000",       // QQ 群号或 QQ 号
+    "count": 100         // 消息总数
+};
---
+++ main.js
@@ -0,0 +1,41 @@
+const QQBot = require('./lib/QQBot.js');
+
+const pluginManager = {
+    log: (message, isError = false) => {
+        let date = new Date().toISOString();
+        let output = `[${date.substring(0,10)} ${date.substring(11,19)}] ${message}`;
+
+        if (isError) {
+            console.error(output);
+        } else {
+            console.log(output);
+        }
+    },
+};
+
+const config = require('./config.js');
+
+let qqbot = new QQBot({
+    host: config.host || '127.0.0.1',
+    port: config.port || 11235
+});
+pluginManager.log('Starting QQBot...');
+
+qqbot.on('Error', (err) => {
+    pluginManager.log(`QQBot Error: ${err.error.toString()} (${err.event})`, true);
+});
+
+qqbot.start();
+
+let penshern = [];
+try {
+    penshern = require('./text.js');
+} catch (ex) {
+    pluginManager.log('Failed to load text.js', true);
+}
+
+for (var num = 1; num <= config.count || 100; num ++) {
+    let random = penshern[Math.floor(Math.random() * penshern.length)]
+    config.isGroup === false ? qqbot.sendPrivateMessage(config.id, random) : qqbot.sendGroupMessage(config.id, random);
+    pluginManager.log('Output: ' + random);
+};
