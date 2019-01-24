36832f9d45994df1d6f30f0081edbd5c557bbd98
mrhso <liu.songquan@live.cn>
2019-01-24 18:46:11 +0800

    将小万邦机器人整合为范一满被动模式
    注意以前的「id」变成了「to」。

--- config.example.js
+++ config.example.js
@@ -4,8 +4,10 @@
 module.exports = {
     "host": "127.0.0.1", // 酷 Q 所在环境的 IP
     "port": 11235,       // 酷 Q 的通信端口
+    "id": "10000",       // 机器人本身的 QQ 号
+    "mode": "active",    // active 为主动打喷，passive 为被动（被 at 和私聊）打喷
     "isGroup": true,     // true 为群，false 为私聊
-    "id": "10000",       // QQ 群号或 QQ 号
+    "to": "10000",       // 目的 QQ 群号或 QQ 号
     "count": 100,        // 消息总数
     "sleep": 100,        // 单字符延时（毫秒），单字符延时 × 字符数 = 字符串延时
     "unique": false      // true 则喷辞不重复（文本用尽后会重置），false 允许重复
--- main.js
+++ main.js
@@ -39,7 +39,7 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 };
 
-async function daapen() {
+async function daapenActive() {
     for (let i = 1; i <= (config.count || 100); i ++) {
         if (config.unique && penshernCopy.length === 0) {
             penshernCopy.push(...penshern);
@@ -48,9 +48,9 @@ async function daapen() {
         let random = penshernCopy[ramdomIndex];
         await sleep((config.sleep || 100) * [...random].length);
         if (config.isGroup === undefined ? true : config.isGroup) {
-            qqbot.sendGroupMessage(config.id, random);
+            qqbot.sendGroupMessage(config.to, random);
         } else {
-            qqbot.sendPrivateMessage(config.id, random);
+            qqbot.sendPrivateMessage(config.to, random);
         };
         pluginManager.log(`Output: ${random}`);
         if (config.unique) {
@@ -59,4 +59,37 @@ async function daapen() {
     };
 };
 
-daapen();
+function daapenPassive() {
+    qqbot.on('GroupMessage', (rawdata) => {
+        if (config.unique && penshernCopy.length === 0) {
+            penshernCopy.push(...penshern);
+        };
+        if (rawdata.extra.ats.indexOf(config.id) > -1) {
+            let ramdomIndex = Math.floor(Math.random() * penshernCopy.length);
+            let random = penshernCopy[ramdomIndex];
+            qqbot.sendGroupMessage(rawdata.group, `[CQ:at,qq=${rawdata.from}] ${random}`, {noEscape: true});
+            pluginManager.log(`Output: @${rawdata.user.groupCard || rawdata.user.name || rawdata.user.qq.toString()} ${random}`);
+        };
+        if (config.unique) {
+            penshernCopy.splice(ramdomIndex, 1);
+        };
+    });
+    qqbot.on('PrivateMessage', (rawdata) => {
+        if (config.unique && penshernCopy.length === 0) {
+            penshernCopy.push(...penshern);
+        };
+        let ramdomIndex = Math.floor(Math.random() * penshernCopy.length);
+        let random = penshernCopy[ramdomIndex];
+        qqbot.sendPrivateMessage(rawdata.from, random);
+        pluginManager.log(`Output: ${random}`);
+        if (config.unique) {
+            penshernCopy.splice(ramdomIndex, 1);
+        };
+    });
+};
+
+if (config.mode === "active") {
+    daapenActive();
+} else if (config.mode === "passive") {
+    daapenPassive();
+};
