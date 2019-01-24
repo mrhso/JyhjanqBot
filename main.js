2580bea167489dcd4d2094df28d0880864c97ba2
mrhso <liu.songquan@live.cn>
2019-01-24 19:05:28 +0800

    改善可读性

--- main.js
+++ main.js
@@ -44,15 +44,21 @@ async function daapenActive() {
         if (penshernCopy.length === 0) {
             penshernCopy.push(...penshern);
         };
+
         let ramdomIndex = Math.floor(Math.random() * penshernCopy.length);
         let random = penshernCopy[ramdomIndex];
-        await sleep((config.sleep || 100) * [...random].length);
+
+        if (config.sleep === undefined ? true : config.sleep) {
+            await sleep((config.sleep || 100) * [...random].length);
+        };
+
         if (config.isGroup === undefined ? true : config.isGroup) {
             qqbot.sendGroupMessage(config.to, random);
         } else {
             qqbot.sendPrivateMessage(config.to, random);
         };
         pluginManager.log(`Output: ${random}`);
+
         if (config.unique) {
             penshernCopy.splice(ramdomIndex, 1);
         };
@@ -64,24 +70,39 @@ function daapenPassive() {
         if (penshernCopy.length === 0) {
             penshernCopy.push(...penshern);
         };
+
         if (rawdata.extra.ats.indexOf(config.id) > -1) {
             let ramdomIndex = Math.floor(Math.random() * penshernCopy.length);
             let random = penshernCopy[ramdomIndex];
+
+            if (config.sleep === undefined ? true : config.sleep) {
+                await sleep((config.sleep || 100) * [...random].length);
+            };
+
             qqbot.sendGroupMessage(rawdata.group, `[CQ:at,qq=${rawdata.from}] ${random}`, {noEscape: true});
             pluginManager.log(`Output: @${rawdata.user.groupCard || rawdata.user.name || rawdata.user.qq.toString()} ${random}`);
         };
+
         if (config.unique) {
             penshernCopy.splice(ramdomIndex, 1);
         };
     });
+
     qqbot.on('PrivateMessage', (rawdata) => {
         if ( penshernCopy.length === 0) {
             penshernCopy.push(...penshern);
         };
+
         let ramdomIndex = Math.floor(Math.random() * penshernCopy.length);
         let random = penshernCopy[ramdomIndex];
+
+        if (config.sleep === undefined ? true : config.sleep) {
+            await sleep((config.sleep || 100) * [...random].length);
+        };
+
         qqbot.sendPrivateMessage(rawdata.from, random);
         pluginManager.log(`Output: ${random}`);
+
         if (config.unique) {
             penshernCopy.splice(ramdomIndex, 1);
         };
