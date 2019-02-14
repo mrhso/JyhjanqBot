84aa1f6ed58940007dabcd0b8817a1558feb3486
mrhso <liu.songquan@live.cn>
2019-02-14 19:01:04 +0800

    迫真名动

--- kufon.format.example.js
+++ kufon.format.example.js
@@ -4,11 +4,11 @@
 // 同样无需考虑 CQ 码
 
 module.exports = [
-    "${arrayRandom(kufonText['双字词'])}，${arrayRandom(kufonText['双字词'])}，${arrayRandom(kufonText['双字词'])}了${arrayRandom(kufonText['双字词'])}。",
+    "${arrayRandom(kufonText['双字名词'])}，${arrayRandom(kufonText['双字名词'])}，${arrayRandom(kufonText['双字动词'])}了${arrayRandom(kufonText['双字名词'])}。",
     "${arrayRandom(kufonText['四字词'])}，${arrayRandom(kufonText['四字词'])}，不过是一场${arrayRandom(kufonText['四字词'])}。",
     "你说${arrayRandom(kufonText['四字词'])}，我说${arrayRandom(kufonText['四字词'])}，最后不过${arrayRandom(kufonText['四字词'])}。",
-    "${arrayRandom(kufonText['双字词'])}，${arrayRandom(kufonText['双字词'])}，许我一场${arrayRandom(kufonText['四字词'])}。",
-    "一${arrayRandom(kufonText['单字'])}一${arrayRandom(kufonText['单字'])}一${arrayRandom(kufonText['双字词'])}，半${arrayRandom(kufonText['单字'])}半${arrayRandom(kufonText['单字'])}半${arrayRandom(kufonText['双字词'])}。",
+    "${arrayRandom(kufonText['双字名词'])}，${arrayRandom(kufonText['双字名词'])}，许我一场${arrayRandom(kufonText['四字词'])}。",
+    "一${arrayRandom(kufonText['单字名词'])}一${arrayRandom(kufonText['单字名词'])}一${arrayRandom(kufonText['双字名词'])}，半${arrayRandom(kufonText['单字名词'])}半${arrayRandom(kufonText['单字名词'])}半${arrayRandom(kufonText['双字名词'])}。",
     "你说${arrayRandom(kufonText['四字词'])}${arrayRandom(kufonText['四字词'])}，后来${arrayRandom(kufonText['四字词'])}${arrayRandom(kufonText['四字词'])}。",
     "${arrayRandom(kufonText['四字词'])}，${arrayRandom(kufonText['四字词'])}，终不敌${arrayRandom(kufonText['四字词'])}。"
 ];
